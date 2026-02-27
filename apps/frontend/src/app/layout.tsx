0867e705373d339e4d51dcd83eab8f1275aaac5d
Bridget <bridgetnamugga@gmail.com>
2026-02-27 13:05:07 +0300

    fix(frontend): uninstalled sonner package

--- apps/frontend/src/app/layout.tsx
+++ apps/frontend/src/app/layout.tsx
@@ -1,5 +1,4 @@
 import { Geist, Geist_Mono } from "next/font/google";
-import { Toaster } from "sonner";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -22,7 +21,6 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         {children}
-         <Toaster richColors  />
       </body>
     </html>
   );
